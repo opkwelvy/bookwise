8ba9e1e0a3a380bcbb55af36dacd0ccd397165e9
opkwelvy <kelvycorrea35@gmail.com>
2024-12-03 18:14:06 -0300

    bookwise

---
+++ livro.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bookwise</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+
+<body class="bg-stone-950 text-stone-200">
+    <header class="bg-stone-900 shadow-lg ">
+        <nav class="mx-auto max-w-screen-lg flex justify-between px-8 py-4">
+            <div class="font-bold text-xl tracking-wide">
+                Book Wise
+            </div>
+            <ul class="flex space-x-4 font-bold">
+                <li><a href="/" class="text-lime-500">Explorar</a></li>
+                <li><a href="/meus-livros.php" class="hover:underline">Meus Livros</a></li>
+            </ul>
+            <ul>
+                <li><a href="/login.php" class="hover:underline">Fazer Login</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main class="mx-auto max-w-screen-lg space-y-10">
+        
+    </main>
+</body>
+
+</html>
