00cc652b0b59c2d6126f8ecc8251bf1dcd46b92c
opkwelvy <kelvycorrea35@gmail.com>
2024-12-04 17:06:52 -0300

    create a MVC estructure

---
+++ dados.php
@@ -0,0 +1,11 @@
+<?php
+
+$livros = [
+    ['id' => 1, 'title' => 'Senhor do Anéis', 'author'=> 'J.R.R. Tolkien', 'description' => 'lorem ipsum'],
+    ['id' => 2, 'title' => '1984', 'author'=> 'Geroge Orwell', 'description' => 'lorem ipsum'],
+    ['id' => 3, 'title' => 'Jogos Vorazes', 'author'=> 'Susan Collins', 'description' => 'lorem ipsum'],
+    ['id' => 4, 'title' => 'Dom Casmurro', 'author'=> 'Machado de Assis', 'description' => 'lorem ipsum'],
+    ['id' => 5, 'title' => 'Senhor do Anéis', 'author'=> 'J.R.R. Tolkien', 'description' => 'lorem ipsum'],
+    ]
+
+?>
--- livro.php
+++ livro.php
@@ -1,3 +1,14 @@
+<?php
+require 'dados.php';
+
+$id = $_REQUEST['id'];
+
+$livroFiltrado = array_filter($livros, function ($l) use ($id) {
+    return $l['id'] == $id;
+});
+
+$livro = array_pop($livroFiltrado);
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -25,7 +36,21 @@
         </nav>
     </header>
     <main class="mx-auto max-w-screen-lg space-y-10">
-        
+        <?php echo $livro['title'] ?>
+        <div class="border-stone-800 border-2 p-2 rounded bg-stone-900">
+            <div class="flex">
+                <div class="w-1/3">imagem</div>
+                <div>
+                    <a href="/livro.php?id=<?= $livro['id'] ?>" class="font-semibold"><?= $livro['title'] ?></a>
+                    <div class="text-xs italic"><?= $livro['author'] ?></div>
+                    <div class="text-xs italisc">avaliação</div>
+                </div>
+
+            </div>
+            <div class="text-sm">
+                <?= $livro['description'] ?>
+            </div>
+        </div>
     </main>
 </body>
 
